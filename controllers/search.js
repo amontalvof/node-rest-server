32a368334aeef14bd86bf6e582d053aaba7daa6f
amontalvof <amontalvof92@gmail.com>
2021-04-27 16:06:54 -0500

    feat: search other collections

--- controllers/search.js
+++ controllers/search.js
@@ -4,7 +4,7 @@ const { User, Category, Product } = require('../models');
 
 const allowedCollections = ['users', 'roles', 'categories', 'products'];
 
-const searchUser = async (term = '', res = response) => {
+const searchUsers = async (term = '', res = response) => {
     const isMongoId = ObjectId.isValid(term);
     if (isMongoId) {
         const user = await User.findById(term);
@@ -29,6 +29,48 @@ const searchUser = async (term = '', res = response) => {
     res.json({ total, results: users });
 };
 
+const searchCategories = async (term = '', res = response) => {
+    const isMongoId = ObjectId.isValid(term);
+    if (isMongoId) {
+        const category = await Category.findById(term).populate('user', 'name');
+        return res.json({
+            results: category ? [category] : [],
+        });
+    }
+
+    const regex = new RegExp(term, 'i');
+
+    const [categories, total] = await Promise.all([
+        Category.find({ name: regex, status: true }).populate('user', 'name'),
+        Category.countDocuments({ name: regex, status: true }),
+    ]);
+
+    res.json({ total, results: categories });
+};
+
+const searchProducts = async (term = '', res = response) => {
+    const isMongoId = ObjectId.isValid(term);
+    if (isMongoId) {
+        const product = await Product.findById(term)
+            .populate('user', 'name')
+            .populate('category', 'name');
+        return res.json({
+            results: product ? [product] : [],
+        });
+    }
+
+    const regex = new RegExp(term, 'i');
+
+    const [products, total] = await Promise.all([
+        Product.find({ name: regex, status: true })
+            .populate('user', 'name')
+            .populate('category', 'name'),
+        Product.countDocuments({ name: regex, status: true }),
+    ]);
+
+    res.json({ total, results: products });
+};
+
 const search = (req, res = response) => {
     const { collection, term } = req.params;
     if (!allowedCollections.includes(collection)) {
@@ -39,13 +81,14 @@ const search = (req, res = response) => {
 
     switch (collection) {
         case 'users':
-            searchUser(term, res);
+            searchUsers(term, res);
             break;
         case 'categories':
+            searchCategories(term, res);
             break;
         case 'products':
+            searchProducts(term, res);
             break;
-
         default:
             res.status(500).json({
                 msg: 'I forgot to do this search',
