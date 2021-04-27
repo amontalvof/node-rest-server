665d1d8173db5c36de3d7d7a9d455feb6bb80a02
amontalvof <amontalvof92@gmail.com>
2021-04-27 13:30:04 -0500

    feat: route to search

--- appServer.js
+++ appServer.js
@@ -11,6 +11,7 @@ class Server {
             auth: '/api/auth',
             categories: '/api/categories',
             products: '/api/products',
+            search: '/api/search',
             users: '/api/users',
         };
 
@@ -42,8 +43,9 @@ class Server {
     routes() {
         this.app.use(this.paths.auth, require('./routes/auth'));
         this.app.use(this.paths.categories, require('./routes/categories'));
-        this.app.use(this.paths.users, require('./routes/users'));
         this.app.use(this.paths.products, require('./routes/products'));
+        this.app.use(this.paths.search, require('./routes/search'));
+        this.app.use(this.paths.users, require('./routes/users'));
     }
 
     listen() {
---
+++ controllers/search.js
@@ -0,0 +1,11 @@
+const { response } = require('express');
+
+const search = (req, res = response) => {
+    const { collection, term } = req.params;
+    res.json({
+        collection,
+        term,
+    });
+};
+
+module.exports = { search };
