367d4e00ae85017d4ffe74bfdbc1aee551e625a3
amontalvof <amontalvof92@gmail.com>
2021-04-27 14:03:18 -0500

    feat: database searches

--- controllers/search.js
+++ controllers/search.js
@@ -1,11 +1,42 @@
 const { response } = require('express');
+const { ObjectId } = require('mongoose').Types;
+const { User, Category, Product } = require('../models');
+
+const allowedCollections = ['users', 'roles', 'categories', 'products'];
+
+const searchUser = async (term = '', res = response) => {
+    const isMongoId = ObjectId.isValid(term);
+    if (isMongoId) {
+        const user = await User.findById(term);
+        return res.json({
+            results: user ? [user] : [],
+        });
+    }
+};
 
 const search = (req, res = response) => {
     const { collection, term } = req.params;
-    res.json({
-        collection,
-        term,
-    });
+    if (!allowedCollections.includes(collection)) {
+        return res.status(400).json({
+            msg: `The allowed collections are: ${allowedCollections}`,
+        });
+    }
+
+    switch (collection) {
+        case 'users':
+            searchUser(term, res);
+            break;
+        case 'categories':
+            break;
+        case 'products':
+            break;
+
+        default:
+            res.status(500).json({
+                msg: 'I forgot to do this search',
+            });
+            break;
+    }
 };
 
 module.exports = { search };
